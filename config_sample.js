8cbff933b590af17b538e7d173607b6fb37522fd
gudbro <kepoinmenfess@gmail.com>
2020-09-27 09:18:18 +0700

    ver1

---
+++ config_sample.js
@@ -0,0 +1,8 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+    consumer_key:         '85ydEkXlBBciiAYLONlPMqVhk',
+    consumer_secret:      'QT2RlCWJCybSM98YCAwmjXpUkPGjJvnVA7vnqputXwAv6qLXR4',
+    access_token:         '1305673102160875523-6bstDMBo5xu0UOdqea3mi5x0uKPSph',
+    access_token_secret:  '7gOQNzknTQDDle6IForuCTpP6WIjRXLh0Y6BrTnSafjAZ'
+  }
+  
